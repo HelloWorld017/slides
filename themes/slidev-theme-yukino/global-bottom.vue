543670c7451074890b7579281c92f125cf3173da
nenw <khi@nenw.dev>
2026-04-01 16:37:36 +0000

    chore(yukino): make slide on top

--- themes/slidev-theme-yukino/global-bottom.vue
+++ themes/slidev-theme-yukino/global-bottom.vue
@@ -1,7 +1,7 @@
 <template>
   <footer
     class="bg-yukino-bg-soft text-yukino-ink-soft absolute right-4 bottom-4 flex items-center gap-4
-      rounded p-2 text-sm"
+      rounded p-2 text-sm z-1"
   >
     <lucide-circle-dashed />
     <div class="flex items-center gap-2">
